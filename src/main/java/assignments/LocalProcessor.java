a8232bae3cf42302496cfb3dd0d05392c691e5d8
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-07-17 20:41:03 +0300

    Update LocalProcessor.java

--- src/main/java/assignments/LocalProcessor.java
+++ src/main/java/assignments/LocalProcessor.java
@@ -1,6 +1,5 @@
 package assignments;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
@@ -18,18 +17,16 @@ import lombok.Setter;
 public class LocalProcessor {
     private String processorName;
     private Long period = 10_000_000_000_000L;
-    private String processorVersion;
+    private StringBuilder processorVersion;
     private Integer valueOfCheap;
-    private Scanner informationScanner;
     private List<String> stringList = new LinkedList<>();
 
-    public LocalProcessor(String processorName, Long period, String processorVersion, Integer valueOfCheap,
-                          Scanner informationscanner, LinkedList<String> stringArrayList) {
+    public LocalProcessor(String processorName, Long period, StringBuilder processorVersion,
+                          Integer valueOfCheap, LinkedList<String> stringArrayList) {
         this.processorName = processorName;
         this.period = period;
         this.processorVersion = processorVersion;
         this.valueOfCheap = valueOfCheap;
-        this.informationScanner = informationscanner;
         this.stringList = stringArrayList;
     }
 
@@ -48,26 +45,21 @@ public class LocalProcessor {
         if (stringList == null) {
             return " ";
         }
-        StringBuilder stringBuilder = new StringBuilder();
         for (String str : stringList) {
-            stringBuilder.append(str).append(" ");
+            processorVersion.append(str).append(" ");
         }
-        processorName = String.valueOf(stringBuilder);
+        processorName = String.valueOf(processorVersion);
         return processorName;
     }
 
     @ReadFullProcessorNameAnnotation
     public void readFullProcessOrName(File file) {
-        StringBuilder stringBuilder = new StringBuilder();
-        try {
-            informationScanner = new Scanner(file);
+        try (Scanner informationScanner = new Scanner(file)){
             while (informationScanner.hasNext()) {
-                stringBuilder.append(informationScanner.nextLine());
+                processorVersion.append(informationScanner.nextLine());
             }
         } catch (FileNotFoundException e) {
             e.fillInStackTrace();
-        } finally {
-            informationScanner.close();
         }
     }
 }
